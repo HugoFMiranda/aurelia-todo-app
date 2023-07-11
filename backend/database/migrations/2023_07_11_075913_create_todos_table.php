e537e9367acae428d5d99fc1f019a69494fc40f2
Hugo Miranda <hugoeu10@gmail.com>
2023-07-11 16:11:58 +0100

    ✨ FEATURE: DB migrations and seeders

---
+++ backend/database/migrations/2023_07_11_075913_create_todos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->unsignedBigInteger('status_id');
+            $table->timestamps();
+
+            $table->foreign('status_id')
+                ->references('id')
+                ->on('todo_statuses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todos');
+    }
+};
